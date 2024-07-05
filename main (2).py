a1745dc6e9c1b3394dc0edb0d84a965319fbfb5e
Li37377 <jelin.subitha@gmail.com>
2024-07-05 20:01:57 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,8 @@
+# Write a Python program that takes multiple inputs from the user until the user enters 'done'. 
+# And then finally prints the values entered as list.
+l=[]
+y=input("enter something")
+while True:
+    l.append(y)
+    if y="done"
+    break
