06fb1fa456bea196621f7dcac2640d0d97a80dc4
Li37377 <jelin.subitha@gmail.com>
2024-07-11 19:32:51 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,11 @@
+#You're creating a program to greet users. Write a function named 'greet' that takes a user's name 
+#as input and prints a personalized greeting like "Hello, (user entered value)!".
+
+def greet():
+    print("Hello",l)
+    
+l=input("Enter your name")
+greet()
+    
+    
+    
